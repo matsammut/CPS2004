1507bbc00332f2538e8ea2b9b6065b4107593a2b
matsammut <matteo.sammut.20@um.edu.mt>
2022-01-02 12:30:30 +0100

    Added method to add nodes

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -3,6 +3,46 @@
 #include <cstring>
 #include <algorithm>
 
+template <typename T>
+class Node{
+    public:
+    T value;
+    Node *next;
+
+    Node(T value){
+        this->value = value;
+    }
+};
+
+template <typename T>
+class DAG{
+    private:
+    int size_;
+    Node<T> *head_ = NULL;
+    Node<T> *tail_ = NULL;
+
+    public:
+    DAG(){
+        // stores how many nodes are in the data structure
+        this->size_ = 0;
+    }
+    void addNode(T value){
+        if (this->head_ == NULL){
+            this->head_ = new Node<T>(value);
+            this->tail_ = this->head_;
+        }
+        else{
+            this->tail_->next = new Node<T>(value);
+            this->tail_ = this->tail_->next;
+        }
+        this->size_ += 1;
+    }
+};
+
 int main(){
+    DAG<int> glist;
+    glist.addNode(5);
+    glist.addNode(2);
     printf("hi");
 }
+
