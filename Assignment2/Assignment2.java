f9c77a636ffb06b97ea6c9319fa1aa0553e029bc
matsammut <matteo.sammut.20@um.edu.mt>
2022-01-09 14:52:24 +0100

    Making a method to approve new users q2

--- Assignment2/Assignment2.java
+++ Assignment2/Assignment2.java
@@ -1,3 +1,4 @@
+import java.util.Vector;
 class User{
     String username;
     String login;
@@ -19,18 +20,24 @@ class User{
     }
 }
 class Trader extends User{
+    boolean approved = false;
     public Trader(String username, String login){
         super(username, login);
+        approvalRequest(approved,username);
     }
 }
 class Administrator extends User{
     public Administrator(String username, String login){
         super(username, login);
     }
+    public boolean approvalRequest(boolean approval, String login){
+        return true;
+    }
 }
 
 public class Assignment2{
  public static void main(String[] args) {
-     System.out.println("helli");
+    Administrator Peter= new Administrator("peter34", "admin");
+    System.out.println(Peter.login);
  }
 }
