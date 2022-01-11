e336e81102895fcf3f771d5322af4dbb2501572e
matsammut <matteo.sammut.20@um.edu.mt>
2022-01-11 20:54:31 +0100

    Overloaded constructer to make big int from str Q3

--- Assignment3/Assignment3.cpp
+++ Assignment3/Assignment3.cpp
@@ -26,14 +26,55 @@ class myuint{
             bits--;
         }
     }
-    myuint(string num){
-        int x = 0;
-        while (!num.empty()){
-            x*=10;
-            x += num[0]-'0';
-            num = num.substr(1);
+    string divideString(string num){
+        int carry = 0;
+        string divided = "";
+        for (size_t i = 0; i < num.length(); i ++){
+            // even
+            if ((num[i] - 48)%2 == 0 ){
+                divided += (((num[i] - 48)/2)+carry)+48;
+                carry = 0;
+            }
+            // odd
+            else{
+                divided += (((num[i] - 48)/2)+carry)+48;
+                carry = 5;
+            }
         }
-        cout << x;
+
+        // remove front character if it is 0
+        if (divided[0] == '0' && divided.length() > 1){
+            divided = divided.substr(1);
+        }
+
+        return divided;
+    }
+    myuint(string num){
+        this->bits = bits;
+        
+        std::vector<bool> stack;
+        while (num != "0"){
+            // odd numbers 1,3,5,7,9
+            if(num.back() == 49 ||num.back() == 51 ||num.back() == 53 ||num.back() == 55 ||num.back() == 57){
+                stack.push_back(true);
+            }
+            else{
+                stack.push_back(false);
+            }
+            num = divideString(num);
+        }
+
+        while(bits>int(stack.size())){
+            this->binary.push_back(false);
+            bits--;
+        }
+
+        for (std::size_t j = (stack.size() - 1); j > 0; j--)
+        {
+            binary.push_back(stack.at(j));
+        }
+        binary.push_back(stack.back());
+
     }
     void printBinary(){
         for (std::size_t j = 0; j < this->binary.size(); j++)
@@ -41,61 +82,62 @@ class myuint{
                 cout << this->binary[j] << " , ";
             }
     }
-    myuint operator + (const myuint& obj){
-        bool objsmaller = false;
-        myuint tmp;
-        int smaller;
-        if (bits > obj.bits){
+    // myuint operator + (const myuint& obj){
+    //     bool objsmaller = false;
+    //     myuint tmp;
+    //     int smaller;
+    //     if (bits > obj.bits){
             
-            smaller = obj.bits;
-            objsmaller = true;
-        }
-        else{
-            smaller = bits;         
-        }
+    //         smaller = obj.bits;
+    //         objsmaller = true;
+    //     }
+    //     else{
+    //         smaller = bits;         
+    //     }
 
         
-        bool carry = false;
-        if(objsmaller == true){
-            for (std::size_t j = 0; j < binary.size(); j++)
-            {
-                if (j<smaller){
-                    if(binary[j] == 0 && obj.binary[j] == 0){
-                        tmp.binary[j] = false;
-                    }
-                    else if(binary[j] == 0 && obj.binary[j] == 0 && carry ==1){
-                        tmp.binary[j] = true;
-                        carry = false;
-                    }
-                    else if((binary[j] == 1 && obj.binary[j] == 0) || (binary[j] == 0 && obj.binary[j] == 1)){
-                        tmp.binary[j] = true;
-                    }
-                    else if((binary[j] == 1 && obj.binary[j] == 1 && carry == 1)){
-                        tmp.binary[j] = true;
-                        carry = 1;
-                    }
-                    else if(binary[j] == 1 && obj.binary[j]==1){
-                        tmp.binary[j] = false;
-                        carry = 1;
-                    }
+    //     bool carry = false;
+    //     if(objsmaller == true){
+    //         for (std::size_t j = 0; j < binary.size(); j++)
+    //         {
+    //             if (j<smaller){
+    //                 if(binary[j] == 0 && obj.binary[j] == 0){
+    //                     tmp.binary[j] = false;
+    //                 }
+    //                 else if(binary[j] == 0 && obj.binary[j] == 0 && carry ==1){
+    //                     tmp.binary[j] = true;
+    //                     carry = false;
+    //                 }
+    //                 else if((binary[j] == 1 && obj.binary[j] == 0) || (binary[j] == 0 && obj.binary[j] == 1)){
+    //                     tmp.binary[j] = true;
+    //                 }
+    //                 else if((binary[j] == 1 && obj.binary[j] == 1 && carry == 1)){
+    //                     tmp.binary[j] = true;
+    //                     carry = 1;
+    //                 }
+    //                 else if(binary[j] == 1 && obj.binary[j]==1){
+    //                     tmp.binary[j] = false;
+    //                     carry = 1;
+    //                 }
                 
-                }
-            }
-        }
+    //             }
+    //         }
+    //     }
     
         
 
 
-        return tmp;
-    }
+    //     return tmp;
+    // }
 };
  
 
 
 int main(){
-    //17498005798264095394980017816940970922825355447145699491406164851279623993595007385788105416184430592
-    myuint<16> i("17498005798264095394980017816940970922825355447145699491406164851279623993595007385788105416184430592");
+    //17498005798264095394980017816940970922825355447145699491406164851279623993595007385788105416184430591
+    // myuint<334> i("17498005798264095394980017816940970922825355447145699491406164851279623993595007385788105416184430591");
     i.printBinary();
     // cout << pow(2,1023);
+
     cout << "\n";
 }
